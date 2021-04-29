7bccd196a97bf974abf0cc956d28f11fc6be705d
Wesley3455 <78345896+Wesley3455@users.noreply.github.com>
2021-04-29 10:50:41 +0200

    Add files via upload

---
+++ proyecto_real_apr27a.ino
@@ -0,0 +1,80 @@
+const int greenLEDPin = 9;
+const int redLEDPin = 11;
+const int blueLEDPin = 10;
+const int Botonpin = 2;
+
+const int redSensorPin = A0;
+const int greenSensorPin = A1;
+const int blueSensorPin = A2;
+
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+bool encenderleds = false;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+pinMode(greenLEDPin, OUTPUT);
+pinMode(redLEDPin, OUTPUT);
+pinMode(blueLEDPin, OUTPUT);
+pinMode(Botonpin, INPUT);
+}
+
+void loop() {
+  leerBoton();
+  // put your main code here, to run repeatedly:
+redSensorValue = analogRead(redSensorPin); 
+delay(5);
+greenSensorValue = analogRead(greenSensorPin); 
+delay(5);
+blueSensorValue = analogRead(blueSensorPin); 
+
+Serial.print("Raw Sensor Values /t Red ");
+Serial.print(redSensorValue);
+Serial.print("t Green: ");
+Serial.print(greenSensorValue);
+Serial.print("t Blue: ");
+Serial.println(blueSensorValue);
+
+if(encenderleds){
+  
+redValue = redSensorValue/4;
+greenValue = greenSensorValue/4;
+blueValue = blueSensorValue/4;
+
+Serial.print("Mapped Sensor Valuues /t Red ");
+Serial.print(redValue);
+Serial.print("/t Green ");
+Serial.print(greenValue);
+Serial.print("/t Blue ");
+Serial.println(blueValue);
+
+analogWrite(redLEDPin, redValue);
+analogWrite(greenLEDPin, greenValue);
+analogWrite(blueLEDPin, blueValue);
+}
+else {
+  analogWrite(redLEDPin, 0);
+analogWrite(greenLEDPin, 0);
+analogWrite(blueLEDPin, 0);
+}
+
+}   
+
+void leerBoton() {
+  int valorBoton = digitalRead(Botonpin); 
+  if (valorBoton == HIGH) {
+    encenderleds = true;
+  }
+  else {
+    encenderleds = false;
+  }
+  
+}
